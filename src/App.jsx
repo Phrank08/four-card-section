9350bf04704c1ac26150715fc886c01b36441029
Phrank08 <frankokey469@gmail.com>
2025-04-14 09:03:59 -0700

    Add Google Fonts and update App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div></div>
+    <section className='bg-[hsl(0, 0%, 100%)]-100 '>
+      <div className='flex flex-col items-center justify-center min-h-screen'>
+        <h2 className=''></h2>
+      </div> 
+
+    </section>
   )
 }
 
