623267dc912f83fdc4affd3794bb4a9bf2005707
Phrank08 <frankokey469@gmail.com>
2025-04-16 10:30:06 -0700

    chore: update dependencies and configure Tailwind CSS
    - Updated package.json to include autoprefixer and postcss, and adjusted tailwindcss version.
    - Modified App.jsx to enhance the UI with a new layout and text.
    - Updated index.css to import Google Fonts and include Tailwind CSS directives.
    - Adjusted vite.config.js to remove unused tailwindcss import.
    - Created postcss.config.js for Tailwind CSS and autoprefixer integration.
    - Added tailwind.config.js to configure Tailwind CSS with custom font family.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
+    <section className="bg-[hsl(0, 0%, 100%)]">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-red-500 text-4xl font-poppins">Reliable, efficient delivery</h1>
+        <h1>Powered by Technology</h1>
+      </div>  
+
+        <p>Our Artificial intelligence powered tools use millions of project data points to ensure that your project is successful</p>
+    </section>
   )
 }
 
